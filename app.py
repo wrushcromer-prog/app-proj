444dc01ff487023243e5c5c0ae9deef949d4b0f2
Rush Cromer <rushcromer@MacBookPro.lan>
2026-06-27 10:52:56 -0400

    Reed Easter Egg

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 client = OpenAI(api_key=os.environ["BASETEN_API_KEY"],
     base_url="https://inference.baseten.co/v1")
 
-st.title("Rush's AI Handbook Assistant")
+st.title("Rush's AI Handbook Assistant v2")
 
 chroma_client = chromadb.PersistentClient(path="./chroma_db")
 collection = chroma_client.get_collection(name="policies")
@@ -26,7 +26,7 @@ if question:
         messages=[
             {
                 "role": "system",
-                "content": "Answer only using the provided handbook context. If the answer is not contained in the context, say you don't know. If the user mentions her name is Katie or says this is Katie or anything to that affect, start your response with a playful greeting like 'Omgggg hey home girl!' and then answer normally."
+                "content": "Answer only using the provided handbook context. If the answer is not contained in the context, say you don't know. If the user mentions her name is Katie or says this is Katie or anything to that affect, start your response with a playful greeting like 'Omgggg hey home girl!' and then answer normally. If the user introduces himself as Reed, say 'Gihhherrrrt! Welcome, Mr. Granbarkebirkebeiner!'"
             },
             {
                 "role": "user",
